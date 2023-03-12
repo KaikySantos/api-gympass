e48289b2004029d8ee26a05b91ab1b0f4b06b2d1
kaikySantos <info.kaiky@gmail.com>
2023-03-12 17:51:53 -0300

    feat: search gym controller

---
+++ src/http/controllers/gyms/search.ts
@@ -0,0 +1,23 @@
+import { FastifyReply, FastifyRequest } from 'fastify'
+import { z } from 'zod'
+import { makeSearchGymsUseCase } from '@/use-cases/factories/make-search-gyms-use-case'
+
+export async function search(request: FastifyRequest, reply: FastifyReply) {
+  const searchGymsQuerySchema = z.object({
+    query: z.string(),
+    page: z.coerce.number().min(1).default(1),
+  })
+
+  const { query, page } = searchGymsQuerySchema.parse(request.query)
+
+  const searchGymUseCase = makeSearchGymsUseCase()
+
+  const { gyms } = await searchGymUseCase.execute({
+    query,
+    page,
+  })
+
+  return reply.status(201).send({
+    gyms,
+  })
+}
