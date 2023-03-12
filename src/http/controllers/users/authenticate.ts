934d2bee0827ebc10c4fa8e8e838a3143f2e422c
kaikySantos <info.kaiky@gmail.com>
2023-03-12 19:47:28 -0300

    feat: role based access control

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -22,10 +22,17 @@ export async function authenticate(
       password,
     })
 
-    const token = await reply.jwtSign({}, { sign: { sub: user.id } })
+    const token = await reply.jwtSign(
+      {
+        role: user.role,
+      },
+      { sign: { sub: user.id } },
+    )
 
     const refreshToken = await reply.jwtSign(
-      {},
+      {
+        role: user.role,
+      },
       { sign: { sub: user.id, expiresIn: '7d' } },
     )
 
