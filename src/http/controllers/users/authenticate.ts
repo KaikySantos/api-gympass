ebd3f565b2e8117ad64bd08e39f274c5de0c8fa4
kaikySantos <info.kaiky@gmail.com>
2023-03-12 19:26:01 -0300

    feat: refresh token

--- src/http/controllers/users/authenticate.ts
+++ src/http/controllers/users/authenticate.ts
@@ -24,9 +24,22 @@ export async function authenticate(
 
     const token = await reply.jwtSign({}, { sign: { sub: user.id } })
 
-    return reply.status(200).send({
-      token,
-    })
+    const refreshToken = await reply.jwtSign(
+      {},
+      { sign: { sub: user.id, expiresIn: '7d' } },
+    )
+
+    return reply
+      .setCookie('refreshToken', refreshToken, {
+        path: '/',
+        secure: true,
+        sameSite: true,
+        httpOnly: true,
+      })
+      .status(200)
+      .send({
+        token,
+      })
   } catch (err) {
     if (err instanceof InvalidCredentialsError) {
       return reply.status(400).send({
