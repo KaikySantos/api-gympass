a329c0c604670d933001d70dc468b1f5ef38f356
kaikySantos <info.kaiky@gmail.com>
2023-03-12 16:11:50 -0300

    feat: test environment

--- vite.config.ts
+++ vite.config.ts
@@ -3,4 +3,7 @@ import tsconfigpaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
   plugins: [tsconfigpaths()],
+  test: {
+    environmentMatchGlobs: [['src/http/controllers/**', 'prisma']],
+  },
 })
